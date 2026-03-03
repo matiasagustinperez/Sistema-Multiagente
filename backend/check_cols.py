0fd8419ea84d21fe70a6cb3c8d15bc485395c69b
Matias Perez <matiasagustinperez@gmail.com>
2026-03-02 22:33:04 -0300

    chore: remove temp check_cols.py

--- backend/check_cols.py
+++
@@ -1,5 +0,0 @@
-import sqlite3
-conn = sqlite3.connect('app/database.db')
-cols = [r[1] for r in conn.execute("PRAGMA table_info(teachers)").fetchall()]
-print("teachers columns:", cols)
-conn.close()
